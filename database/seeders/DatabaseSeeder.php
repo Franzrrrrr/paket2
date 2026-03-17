dbba5811e31d812fad63e7040408db56b61b4de0
Franzrrrrr <imauel.steven35@smk.belajar.id>
2026-03-17 11:43:59 +0700

    add Sh

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ShieldGenerateSeeder;
 
 class DatabaseSeeder extends Seeder
 {
